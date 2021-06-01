c8c39b71cf80a387c5b5a712af317dface1db36c
sir-shalahuddin <sirajshalahuddin@gmail.com>
2021-06-01 08:24:47 +0700

    membahkan isi file penjumlahan

---
+++ lib/penjumlahan.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'tape.dart';
+
+class Penjumlahan extends StatefulWidget {
+  @override
+  _PenjumlahanState createState() => _PenjumlahanState();
+}
+
+class _PenjumlahanState extends State<Penjumlahan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
